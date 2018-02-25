51beb2d3206483e35d569c7615d42d29f690c38f
MickDavies <mickeyjdavies@gmail.com>
2018-02-25 22:29:33 +0000

    Add GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,12 +3,15 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectId} = require('mongodb')
 
 const todos = [
   {
+    _id: new ObjectId(),
     text: 'first test todo'
   },
   {
+    _id: new ObjectId(),
     text: 'second test todo'
   }
 ]
@@ -73,4 +76,30 @@ describe('GET /todos', () => {
       })
       .end(done);
   })
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${new ObjectId().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    request(app)
+      .get(`/todos/dfsdf`)
+      .expect(404)
+      .end(done);
+  });
 })
